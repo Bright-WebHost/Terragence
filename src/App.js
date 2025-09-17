b36288c0c225057dc7283633d9c929dcfc83d771
Mohammed Junaid <mohadjunaid212@gmail.com>
2025-09-17 12:28:21 +0530

    / add

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ export default function  App() {
   <NavBar/>
   {/* <Layout/> */}
         <Routes>
-          <Route path=""  element={<Home/>} />
+          <Route path="/"  element={<Home/>} />
           <Route path="/products" element={<Products />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact/>} />
